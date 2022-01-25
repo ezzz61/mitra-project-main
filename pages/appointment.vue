5ef73bddc8998cd75f20da265b91db2ef7792705
Chandra Riyadi <chandra.riyadi@raharja.info>
2022-01-25 13:10:24 +0700

    Add news pages

--- pages/appointment.vue
+++ pages/appointment.vue
@@ -3,8 +3,9 @@
     <Navigation/>
       <header class="w-11/12 lg:w-10/12 3xl:container mx-auto">
         <div class="">
-          <div class="flex justify-between items-center w-full p-3 rounded-2xl bg-sky-600">
-            <div class="">
+
+          <div class="flex justify-between items-center w-full p-6 rounded-2xl bg-sky-600">
+            <div class="w-1/4">
               <h2 class="text-lg text-white mb-10">APPOINTMENT</h2>
               <form class="">
               <div class="block">
@@ -17,13 +18,13 @@
               </div>
               <div class="block mt-3">
                 <span class="text-white">Email</span>
-                <input type="email" class="block w-ful mt-1 outline-none p-1">
+                <input type="email" class="block w-full mt-1 outline-none p-1">
               </div>
               <div class="block mt-3">
                 <span class="text-white">Tanggal</span>
                 <input type="date" class="block w-full mt-1 outline-none p-1">
               </div>
-              <div>
+              <div class="flex justify-center gap-x-4">
               <button class="p-2 mt-10 rounded bg-gray-50">Back</button>
               <button class="p-2 mt-10 rounded text-white bg-sky-800">Submit</button>
               </div>
