722acbb57a51aeb68f4c6fae08c84ac750cee897
Giaz Pawaz <78998804+Giaz18@users.noreply.github.com>
2022-03-08 22:54:53 +0700

    PUKIMAK

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,8 +65,8 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: "http://localhost:8080",
-    // baseURL: "https://trimitra-server.herokuapp.com",
+    // baseURL: "http://localhost:8080",
+    baseURL: "https://trimitra-server.herokuapp.com",
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
